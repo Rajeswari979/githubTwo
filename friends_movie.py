9ff7dab1b07d21e6954b35ed7a9f17e27af85a22
Rajeswari979 <rajeswarim979@gmail.com>
2024-02-29 22:40:38 +0530

    in process

---
+++ friends_movie.py
@@ -0,0 +1,7 @@
+pur=["gargi","vetri","tamil","marvel","jeo"]
+asmi=["tamil","bigil","marvel","iron","parking"]
+sowmi=["love today","marvel","darbar","tamil","singam"]
+me=["tami","varisu","love today","rrr","neeyum nanum"]
+
+if pur in asmi:
+    print("ef")
